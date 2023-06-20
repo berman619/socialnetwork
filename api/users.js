e8ffc4800d5b89e6d7d877c56a410c973dfb0c24
Zach Berger <zachhberger@gmail.com>
2023-06-20 17:32:33 -0400

    more pseudocode for user routes

--- api/users.js
+++ api/users.js
@@ -1,16 +1,27 @@
-// GET all users: 
-    // Use the .find() method on the User model.
+// /api/users endpoint
 
-// GET a single user by its _id and populated thought and friend data: 
-    // Use the .findOne() method with the id parameter.
-    // To populate the thoughts and friends data, use the .populate() method.
+    // GET all users: 
+        // Use the .find() method on the User model.
 
-// POST a new user: 
-    // Use the .create() method with the request body as the argument.
+    // GET a single user by its _id and populated thought and friend data: 
+        // Use the .findOne() method with the id parameter.
+        // To populate the thoughts and friends data, use the .populate() method.
 
-// PUT to update a user by its _id: 
-    // Use the .findOneAndUpdate() method with the id parameter and request body as the arguments.
+    // POST a new user: 
+        // Use the .create() method with the request body as the argument.
 
-// DELETE to remove user by its _id: 
-    // Use the .findOneAndDelete() method with the id parameter as the argument. 
-    // For the bonus part, find associated thoughts and delete them as well.
+    // PUT to update a user by its _id: 
+        // Use the .findOneAndUpdate() method with the id parameter and request body as the arguments.
+
+    // DELETE to remove user by its _id: 
+        // Use the .findOneAndDelete() method with the id parameter as the argument. 
+        // For the bonus part, find associated thoughts and delete them as well.
+
+// /api/users/:userId/friends/:friendId endpoint
+
+    // POST to add a new friend to a user's friend list: 
+        // Find the user by id and update their friends list to include the new friend id. 
+        // Use the .findOneAndUpdate() method with the user id and friend id as arguments.
+
+    // DELETE to remove a friend from a user's friend list: 
+        // Similar to above but remove the friend id from the friends list.
