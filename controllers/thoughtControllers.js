433ed97b8eb684a5ee4333aa99c70f1fd039fafe
Zach Berger <zachhberger@gmail.com>
2023-06-21 15:26:07 -0400

    building out thought controller

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,7 +1,5 @@
 const { Thought } = require('./models');
 
-// /api/thoughts
-
 module.exports = {
     async getThoughts(req, res) {
       try {
@@ -14,11 +12,11 @@ module.exports = {
 
     async getSingleThought(req, res) {
         try {
-          const thought = await User.findById(req.params.thoughtId)
+          const thought = await Thought.findById(req.params.thoughtId)
           if (!thought) {
             return res.status(404).json({ message: 'Thought not found' });
           }
-          res.json(user);
+          res.json(thought);
         } catch (error) {
           res.status(500).json({ message: 'Server error' });
         }
@@ -28,10 +26,9 @@ module.exports = {
         try {
           const dbThoughtData = await Thought.create(req.body);
           
-          // Update the associated user's thoughts array
-          const user = await User.findById(dbThoughtData.userId);
-          if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+          const thought = await Thought.findById(dbThoughtData.thoughtId);
+          if (!thought) {
+            return res.status(404).json({ message: 'Thought not found' });
           }
           user.thoughts.push(dbThoughtData._id);
           await user.save();
@@ -40,15 +37,75 @@ module.exports = {
         } catch (error) {
           res.status(500).json({ message: 'Server error' });
         }
-      }
+      },
 
+      async updateThought(req, res) {
+        try {
+          const updatedThought = await Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            req.body,
+            { new: true }
+          );
+    
+          if (!updatedThought) {
+            return res.status(404).json({ message: 'Thought not found' });
+          }
+    
+          res.json(updatedThought);
+        } catch (error) {
+          res.status(500).json({ message: 'Server error' });
+        }
+      },
 
-// PUT to update a thought by its _id: Use the .findOneAndUpdate() method with the id parameter and request body as the arguments.
+      async deleteThought(req, res) {
+        try {
+          const deletedThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+    
+          if (!deletedThought) {
+            return res.status(404).json({ message: 'Thought not found' });
+          }
+    
+          res.json({ message: 'Thought deleted successfully' });
+        } catch (error) {
+          res.status(500).json({ message: 'Server error' });
+        }
+      },
 
-// DELETE to remove a thought by its _id: Use the .findOneAndDelete() method with the id parameter as the argument.
-
-// /api/thoughts/:thoughtId/reactions
-
-// POST to create a reaction stored in a single thought's reactions array field: Find the thought by id and update their reactions array to include the new reaction. You can use the .findOneAndUpdate() method with the thought id and reaction data as arguments.
-
-// DELETE to pull and remove a reaction by the reaction's reactionId value: Find the thought by id and remove the reaction by its id from the reactions array.
+      async createReaction(req, res) {
+        try {
+          const thoughtId = req.params.thoughtId;
+          const { reactionBody, username } = req.body;
+      
+          const thought = await Thought.findById(thoughtId);
+          if (!thought) {
+            return res.status(404).json({ message: 'Thought not found' });
+          }
+      
+          thought.reactions.push({ reactionBody, username });
+          const updatedThought = await thought.save();
+      
+          res.json(updatedThought);
+        } catch (error) {
+          res.status(500).json({ message: 'Server error' });
+        }
+      },
+      
+      async deleteReaction(req, res) {
+        try {
+          const thoughtId = req.params.thoughtId;
+          const reactionId = req.params.reactionId;
+      
+          const thought = await Thought.findById(thoughtId);
+          if (!thought) {
+            return res.status(404).json({ message: 'Thought not found' });
+          }
+      
+          thought.reactions.pull({ _id: reactionId });
+          const updatedThought = await thought.save();
+      
+          res.json(updatedThought);
+        } catch (error) {
+          res.status(500).json({ message: 'Server error' });
+        }
+      },
+};
